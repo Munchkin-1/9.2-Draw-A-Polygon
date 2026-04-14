810539a0331df37054ecc8c1b0ca8244ef6bacd1
Phoenix <trumanparks@gmail.com>
2026-04-14 15:44:58 -0500

    finished the first round of the "draw" functions, have not tested them yet, and have not clened up commented out old code.

---
+++ animation.py
@@ -0,0 +1,78 @@
+from turtle import *
+import random
+
+
+
+
+def gen_color():
+    return f"#{random.randint(0, 0xFFFFFF):06x}"
+
+def play_area():
+        
+    pen = Turtle()
+    # pen = Turtle.color(gen_color)
+    pen.speed(0)
+    pen.penup()
+    pen.color("white")
+    pen.hideturtle()
+    
+    pen.begin_fill()
+    pen.goto(-240,240)
+    pen.goto(-240,-240)
+    pen.goto(240,-240)
+    pen.goto(240,240)
+    pen.goto(-240,240)
+    pen.end_fill()
+
+def move_forward(turt):
+    turt.forward(5)
+
+    if turt.xcor() > 235 or turt.xcor() < -235: 
+        turt.speed(0)
+        turt.setheading(180- turt.heading()) 
+        turt.forward(10)  
+        turt.speed(.5)
+    if turt.ycor() > 235 or turt.ycor() <-235:
+        yert.speed(0)
+        turt.setheading(0 - turt.heading())
+        turt.forward(10)
+        yert.speed(.5)
+
+def move_xy(turt, deltaX, deltaY):
+    newX = turt.xcor() + deltaX
+    newY = turt.ycor() + deltaY
+
+    #out of bounds or right or left
+    if newX > 240 or newX < -240:
+        deltaX *= -1
+        newX = turt.xcor()
+    #out of bounds top or bottom    
+    if newY > 240 or newY < -240:
+        deltaY *= -1
+        newY = turt.ycor()
+
+    turt.goto(newX, newY)
+    return deltaX, deltaY
+
+screen = Screen()
+screen.bgcolor("black")
+screen.setup(500, 500)
+
+play_area()
+
+yert = Turtle()
+yert.shape("triangle")
+yert.setheading(random.randint(0, 360))
+yert.color(gen_color())
+deltaX = random.randint(-5,5)
+deltaY = random.randint(-5,5)
+
+# while True:
+#     move_forward(yert)
+    
+while True:
+    deltaX, deltaY = move_xy(yert, deltaX, deltaY)
+
+
+screen.exitonclick()
+
--- main.py
+++ main.py
@@ -3,15 +3,15 @@ screen = Screen()
 screen.bgcolor("teal")
 screen.setup(400, 400)
 
-def regular_polygon(turtle, sides):
-    turtle.begin_fill()
-    for i in range(sides):
-        turtle.forward(100/sides)
-        turtle.right(360/sides)
-    turtle.end_fill()
+# def regular_polygon(turtle, sides):
+#     turtle.begin_fill()
+#     for i in range(sides):
+#         turtle.forward(100/sides)
+#         turtle.right(360/sides)
+#     turtle.end_fill()
 
-# def draw():
-#     sides = int(input("How many sides does your shape have?"))
+# # def draw():
+# #     sides = int(input("How many sides does your shape have?"))
 
 
 pen = Turtle()
@@ -19,14 +19,64 @@ pen.speed(0)
 pen.color("white")
 pen.hideturtle()
 
-regular_polygon(pen, 3)
+# regular_polygon(pen, 3)
 
-while True:
-    sides = int(input("How many sides does the polygon have? "))
-    pen.clear()
-    if sides == 3:
-        regular_polygon(pen, sides)
-        name.write("TRIANGLE", font = ("Times New Roman", 20))
+# while True:
+#     sides = int(input("How many sides does the polygon have? "))
+#     pen.clear()
+#     if sides == 3:
+#         regular_polygon(pen, sides)
+#         name.write("TRIANGLE", font = ("Times New Roman", 20))
 
 
-screen.exitonclick()
+# screen.exitonclick()
+
+
+# import turtle
+
+# # Setup the screen and turtle
+# screen = Turtle.Screen()
+# t = turtle.Turtle()
+# t.speed(3)
+
+def draw_regular_polygon(sides, length):
+    
+    angle = 360 / sides
+    for _ in range(sides):
+        pen.forward(length)
+        pen.left(angle)
+
+def draw_rectangle(width, height):
+    
+    for _ in range(2):
+        pen.forward(width)
+        pen.right(90)
+        pen.forward(height)
+        pen.right(90)
+
+def draw_parallelogram(side1, side2, angle):
+    for _ in range(2):
+        pen.forward(side1)
+        pen.right(angle)
+        pen.forward(side2)
+        pen.right(180 - angle)
+
+def draw_trapezoid():
+    
+    pen.forward(150) 
+    pen.left(120)
+    pen.forward(60)
+    pen.left(60)
+    pen.forward(90)  
+    pen.left(60)
+    pen.forward(60)
+    pen.setheading(0)
+
+def draw_irregular_quad():
+    pen.forward(100)
+    pen.right(110)
+    pen.forward(80)
+    pen.right(100)
+    pen.forward(110)
+    pen.goto(0, 0) 
+    pen.setheading(0)
